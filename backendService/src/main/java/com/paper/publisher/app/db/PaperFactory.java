dad094c11d871e73cc86c795d8d7ca7c0635d044
Devin Riess <devin.riess@colorado.edu>
2024-05-31 10:49:26 -0600

    relative file path and proper encoding to base64

--- backendService/src/main/java/com/paper/publisher/app/db/PaperFactory.java
+++ backendService/src/main/java/com/paper/publisher/app/db/PaperFactory.java
@@ -1,19 +1,22 @@
 package com.paper.publisher.app.db;
 
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.Reader;
+import java.io.File;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.util.Base64;
+
+import org.apache.commons.io.FileUtils;
 
 public class PaperFactory {
 
     private void insertPaper(String filename, String id) {
         JDBCTemplate jdbcTemplate = new JDBCTemplate();
-        Reader reader;
+        File file;
         try {
-            reader = new FileReader(filename);
+            file = new File(filename);
+            String encodedFile = Base64.getEncoder().encodeToString(FileUtils.readFileToByteArray(file));
             try {
                 Connection conn = jdbcTemplate.getConnection();
 				String sql = "UPDATE paper " +
@@ -21,20 +24,20 @@ public class PaperFactory {
                             "WHERE id = '" + id + "';";
 
                 PreparedStatement stmt = conn.prepareStatement(sql);
-                stmt.setClob(1, reader);
+                stmt.setString(1, encodedFile);
 				stmt.executeUpdate();
             } catch (SQLException ex) {
 				ex.printStackTrace();
             }
-        } catch (FileNotFoundException e) {
+        } catch (IOException e) {
             // TODO Auto-generated catch block
-            e.printStackTrace();
+
         }
     }
 
     public void createPapers() {
-        insertPaper("D:\\Raytheon Training\\Paper_Publisher\\backendService\\src\\main\\java\\com\\paper\\publisher\\app\\db\\PineapplePizza.txt", "b7825d87-568f-46f8-ac70-8c4bce5feb59");
-        insertPaper("D:\\Raytheon Training\\Paper_Publisher\\backendService\\src\\main\\java\\com\\paper\\publisher\\app\\db\\TheUniverse.txt", "a0ee4ae1-8895-4581-9a07-3dcc64698bf9");
-        insertPaper("D:\\Raytheon Training\\Paper_Publisher\\backendService\\src\\main\\java\\com\\paper\\publisher\\app\\db\\TheOdyssey.txt", "111c36e2-f750-4bfa-b206-f8ff0944444c");
+        insertPaper("src"+ File.separator + "main" + File.separator +"java" + File.separator +"com" + File.separator +"paper"+ File.separator + "publisher"+ File.separator + "app"+ File.separator + "db"+ File.separator + "PineapplePizza.txt", "b7825d87-568f-46f8-ac70-8c4bce5feb59");
+        insertPaper("src"+ File.separator + "main" + File.separator +"java" + File.separator +"com" + File.separator +"paper"+ File.separator + "publisher"+ File.separator + "app"+ File.separator + "db"+ File.separator + "TheUniverse.txt", "a0ee4ae1-8895-4581-9a07-3dcc64698bf9");
+        insertPaper("src"+ File.separator + "main" + File.separator +"java" + File.separator +"com" + File.separator +"paper"+ File.separator + "publisher"+ File.separator + "app"+ File.separator + "db"+ File.separator + "TheOdyssey.txt", "111c36e2-f750-4bfa-b206-f8ff0944444c");
     }
 }
